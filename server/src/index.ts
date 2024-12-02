6bf2e14522c8c97746099c9c4ee4f92584f4ed25
vestabeats <132943841+vestabeats@users.noreply.github.com>
2024-12-02 14:42:09 +0100

    Update index.ts

--- server/src/index.ts
+++ server/src/index.ts
@@ -10,7 +10,11 @@ dotenv.config()
 dbConnection()
 const PORT = process.env.PORT || 5000
 const app = express()
-app.use(cors())
+app.use(cors({
+    origin: '*', // Allow any origin
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allow specific HTTP methods
+    allowedHeaders: ['Content-Type', 'Authorization'], // Specify allowed headers
+}));
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 app.use(cookieParser())
