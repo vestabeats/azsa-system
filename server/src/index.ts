d84c8fbd3fec47c75ac477c8d0c14909a12aad1c
vestabeats <132943841+vestabeats@users.noreply.github.com>
2024-12-02 19:34:56 +0100

    Update index.ts

--- server/src/index.ts
+++ server/src/index.ts
@@ -10,7 +10,11 @@ dotenv.config()
 dbConnection()
 const PORT = process.env.PORT || 5000
 const app = express()
-app.use(cors())
+app.use(cors({
+    origin:"https://azsa-system.onrender.com",
+    methods:["GET","POST","PUT","DELETE"],
+    credentials:true
+}))
 
 // Handle preflight requests (OPTIONS)
 app.options('*', cors({
