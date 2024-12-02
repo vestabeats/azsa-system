0e523c77fba07755c87d9bead3a6f11a26e55302
vestabeats <132943841+vestabeats@users.noreply.github.com>
2024-12-02 15:12:35 +0100

    Update apiSlice.ts

--- client/src/redux/slices/apiSlice.ts
+++ client/src/redux/slices/apiSlice.ts
@@ -1,5 +1,5 @@
 import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react'
-const API_URI = 'http://localhost:8800/api'
+const API_URI = 'https://azsa-system-backend.onrender.com/api'
 //const API_URI = import.meta.env.VITE_APP_BASE_URL
 const baseQuery = fetchBaseQuery({baseUrl:API_URI})
 export const apiSlice = createApi({
