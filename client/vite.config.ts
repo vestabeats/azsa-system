30e5ac8297c064751bcad345e14405957dc842ee
vestabeats <132943841+vestabeats@users.noreply.github.com>
2024-12-02 16:41:45 +0100

    Update vite.config.ts

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
     port:3000,
     proxy:{
       "/api":{
-        target:"http://localhost:8800",
+        target:"https://azsa-system-backend.onrender.com",
         changeOrigin:true
       }
     }
