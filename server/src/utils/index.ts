7e230b373d46c0a0eb77aa752161010816faa25c
vestabeats <132943841+vestabeats@users.noreply.github.com>
2024-12-02 08:57:42 +0100

    Update index.ts

--- server/src/utils/index.ts
+++ server/src/utils/index.ts
@@ -17,7 +17,7 @@ export const createJWT = (res: Response, userId: any): void => {
     const token = jwt.sign({ userId }, process.env.JWT_SECRET as string, { expiresIn: "1d" });
     res.cookie("token", token, {
         httpOnly: true,
-        secure: process.env.NODE_ENV !== "development",
+        secure: process.env.NODE_ENV !== "production",
         sameSite:"strict" ,// csrf attacks
         maxAge: 1*24*60*60*1000
     })
